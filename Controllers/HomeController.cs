a9e925489f660c0636da0b6688757a27d8981afa
Neelam7982 <100210920+Neelam7982@users.noreply.github.com>
2022-03-15 18:33:50 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using My_first_Mvc_Program.Models;
+using My_first_Mvc_Program.My_DAta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace My_first_Mvc_Program.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            empEntities obj = new empEntities();
+            var res = obj.Table_Emp.ToList();
+            return View(res);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/our_View_Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace My_first_Mvc_Program.Models
+{
+    public class our_View_Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
